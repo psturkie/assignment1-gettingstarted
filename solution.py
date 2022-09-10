4ff79bc7a56404c1e10a626a3660d80a91864086
SturkALurk <112786154+psturkie@users.noreply.github.com>
2022-09-10 17:40:41 -0500

    Add files via upload

--- solution.py
+++ solution.py
@@ -7,7 +7,7 @@ def welcome_assignment_answers(question):
     #The student doesn't have to follow the skeleton for this assignment.
     #Another way to implement is using a "case" statements similar to C.
     if question == "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA":
-        answer =="mtls"
+        answer ="mtls"
     elif question == "Are encoding and encryption the same? - Yes/No":
         answer = "No"
     elif question == "Is it possible to decrypt a message without a key? - Yes/No":
@@ -15,9 +15,9 @@ def welcome_assignment_answers(question):
     elif question == "Is it possible to decode a message without a key? - Yes/No":
         answer = "Yes"
     elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
-        answer == "No"
+        answer = "No"
     elif question == "What is the SHA1 hashing value to the following message: 'NYU Computer Networking' - Use SHA1 hash generator and use the answer in your code":
-        answer == "8496abe9fceb5aa927e28bfbd9a2347d1290ef9b"
+        answer = "8496abe9fceb5aa927e28bfbd9a2347d1290ef9b"
     elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
         answer = "No"
     elif question == "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
