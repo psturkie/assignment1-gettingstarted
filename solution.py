c831a6c55ef7ffc06dc175bb3fb1d94f69b47e7c
SturkALurk <112786154+psturkie@users.noreply.github.com>
2022-09-10 15:03:27 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -8,15 +8,7 @@ def welcome_assignment_answers(question):
     if question == "In Slack, what is the secret passphrase posted in the #lab-python-getting started channel posted by a TA?":
         answer ="mtls"
     elif question == "Are encoding and encryption the same? - Yes/No":
-        answer = "No"
-    elif question == "Is it possible to decrypt a message without a key? - Yes/No":
-        answer = "No"
-        elif question == "Is it possible to decode a message without a key? - Yes/No":
-        answer = "Yes"
-        elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
-            answer = "No"
-            elif question == "What is the SHA1 hashing value to the following message: 'NYU Computer Networking' - Use SHA1 hash generator and use the answer in your code":
-                answer = "8496abe9fceb5aa927e28bfbd9a2347d1290ef9b"
+    
     else: 
         ### you should understand why this else case should be included
         ### what happens if there is a typo in one of the questions?
