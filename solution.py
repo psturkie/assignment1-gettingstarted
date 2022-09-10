f5b5d962bad4a96dbf693aacffbd7f66c9143997
SturkALurk <112786154+psturkie@users.noreply.github.com>
2022-09-10 18:05:19 -0500

    Add files via upload

--- solution.py
+++ solution.py
@@ -15,7 +15,7 @@ def welcome_assignment_answers(question):
     elif question == "Is it possible to decode a message without a key? - Yes/No":
         answer = "Yes"
     elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
-        answer = "Yes"
+        answer = "No"
     elif question == "What is the SHA1 hashing value to the following message: 'NYU Computer Networking' - Use SHA1 hash generator and use the answer in your code":
         answer = "8496abe9fceb5aa927e28bfbd9a2347d1290ef9b"
     elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
@@ -24,7 +24,7 @@ def welcome_assignment_answers(question):
         answer = 5
     elif question == "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
         answer = 3
-    return(answer)
+    return answer
 # Complete all the questions.
 
 
